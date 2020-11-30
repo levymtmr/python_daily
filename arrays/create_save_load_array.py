9606d62b809528d1d1507431bbf4d3d4d1f5508d
Levy Mota <levy.mota@oowlish.com>
2020-11-30 10:13:05 -0300

    day 09, add array scripts, about create, save and load.

---
+++ arrays/create_save_load_array.py
@@ -0,0 +1,23 @@
+'''
+Create, save and load array with float points.
+'''
+
+from array import array
+from random import random
+
+floats = array('d', (random() for i in range(10**7)))
+
+print('floats: ', floats[-1])
+
+fp = open('floats.bin', 'wb')
+floats.tofile(fp)
+fp.close()
+
+floats2 = array('d')
+fp = open('floats.bin', 'rb')
+floats2.fromfile(fp, 10**7)
+fp.close()
+
+print('floats2: ', floats2[-1])
+
+print('floats == floats2: ', floats == floats2)
